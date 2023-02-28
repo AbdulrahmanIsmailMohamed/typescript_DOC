42b03e04511782f31e029e3062ee6fb904f09c78
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2023-02-28 16:43:23 +0200

    Type Annotations With Array

---
+++ dist/typeAnnotationsWithArr.js
@@ -0,0 +1,11 @@
+"use strict";
+let myinfo;
+myinfo = 10;
+console.log(myinfo);
+myinfo = "abdo";
+console.log(myinfo);
+let info = ["Ahmed", "Mohammed", "Abdo"];
+for (let i = 0; i < info.length; i++) {
+    console.log(info[i].repeat(3));
+}
+//# sourceMappingURL=typeAnnotationsWithArr.js.map
---
+++ src/typeAnnotationsWithArr.ts
@@ -0,0 +1,11 @@
+let myinfo: string | number;
+myinfo = 10;
+console.log(myinfo);
+myinfo = "abdo";
+console.log(myinfo);
+
+
+let info: string[] = ["Ahmed", "Mohammed", "Abdo"];
+for (let i = 0; i < info.length; i++) {
+  console.log(info[i].repeat(3));
+}
