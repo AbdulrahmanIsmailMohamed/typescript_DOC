1b42ea2c8c3aa32fc23a91b6b5238cd97e4a39b5
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2023-03-11 21:54:31 +0200

    Interface

--- dist/interface/methodsAndParam.js
+++ dist/interface/methodsAndParam.js
@@ -2,4 +2,13 @@
 /**
 * * Interface Method And Parameters
 */
+let myInfos = {
+    name: "Abdulrahman",
+    address: "zagazig",
+    phone: "0156456",
+    zipCode: 101556,
+    country: "Egypt",
+    telephone: "8656454",
+    birthYear: 2001,
+};
 //# sourceMappingURL=methodsAndParam.js.map
