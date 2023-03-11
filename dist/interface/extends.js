e992b3a17bd3153deab318b9112114ad40d7a440
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2023-03-11 21:54:51 +0200

    Interfaces

---
+++ dist/interface/extends.js
@@ -0,0 +1,10 @@
+"use strict";
+/**
+* * Extending Interfaces
+*/
+let information = {
+    name: "Abdulrahman",
+    age: 21,
+    phone: "016456456455"
+};
+//# sourceMappingURL=extends.js.map
---
+++ dist/interface/reOpen.js
@@ -0,0 +1,12 @@
+"use strict";
+/**
+* * Interface Re-Open And Use Cases
+*/
+let userSetting = {
+    theme: true,
+    color: "red",
+    sidebar: false
+};
+userSetting.theme = false;
+// userSetting.color = "green"
+//# sourceMappingURL=reOpen.js.map
